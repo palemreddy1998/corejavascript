7aac1557da44666109bbc88af25c1654cfd29eb5
palemreddy <palemreddy1998@gmail.com>
2017-08-03 09:06:46 +0100

    Intersection of two arrays with es6 map.

---
+++ puzzles/general/IntersectionOfTwoArrays.js
@@ -0,0 +1,14 @@
+var firstArray = [2, 2, 4, 1];
+var secondArray = [1, 2, 0, 2];
+
+intersection(firstArray, secondArray);
+
+function intersection(firstArray,secondArray) {
+
+    var intersectionMap = new Map();
+
+    firstArray.forEach(function (element) {
+        console.log(element)
+
+    });
+}
