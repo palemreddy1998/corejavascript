f96c46019b403368c8b8cd7373b3809b16761961
palemreddy <palemreddy1998@gmail.com>
2017-08-17 09:27:18 +0100

    updated with weakset and weakmap.

--- es6/mapsandsets/Map1.js
+++ es6/mapsandsets/Map1.js
@@ -1,11 +1,15 @@
 let m = new Map()
+//let m = new WeakMap()
 let s = Symbol()
-m.set("alex", 42)
-m.set("charlie", 32)
+m.set("Palem", 42)
+m.set("Reddy", 32)
 m.size === 2
-for (let [ key, val ] of m.entries())
-    console.log(key+" = "+val)
+//for (let [ key, val ] of m.entries())
+//    console.log(key+" = "+val)
 
 m.forEach(function iter1(elementKey,elementValue) {
     console.log(elementKey+" = "+elementValue)
 })
+
+
+console.log(m.entries());
--- es6/mapsandsets/Set1.js
+++ es6/mapsandsets/Set1.js
@@ -1,11 +1,22 @@
-let s = new Set()
-s.add("hello").add("goodbye").add("hello")
+//let s = new Set();
+let s = new WeakSet();
+//works with only set
+//s.add(1).add(2).add("three")
+//s.add("hello").add("goodbye").add("hello")
+
+//works with only weakset
+let value1 = {foo:1}
+let value2 = {bar:1}
+s.add(value1).add(value2)
 s.size === 2
 s.has("hello") === true
-for (let key of s.values()) { // insertion order
-    console.log(key)
-}
+//for (let key of s.values()) { // insertion order
+//    console.log(key)
+//}
 
-s.forEach(function iter1(element) {
-    console.log(element);
-})
+//s.forEach(function iter1(element) {
+    //console.log(element);
+//})
+
+console.log(s.has(value1))
+console.log(s.has(value2))
