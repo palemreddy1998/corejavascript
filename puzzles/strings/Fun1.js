da713e7faaf69c2dc2ff9055c3c6508b60191aed
PalemReddy <palemreddy1998@gmail.com>
2017-08-23 11:48:55 +0100

    Fun1.js
    function with no return type

---
+++ puzzles/strings/Fun1.js
@@ -0,0 +1,6 @@
+function f1(){
+  
+}
+
+console.log(f1);
+//By default, functions return undefined. To return any other value, the function must have a return statement that specifies the value to //return.
